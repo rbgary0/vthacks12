dc1e995625f30c1c84ea75d7fb2e0070746a9054
Rajarshi Tummala <tummalarajarshi1@gmail.com>
2024-09-14 17:19:13 -0400

    api update

--- src/main.py
+++ src/main.py
@@ -1,27 +1,14 @@
-import JsonHandler, DatabaseMD
+import DatabaseMD
+from fastapi import FastAPI
+app = FastAPI()
 
-class main():
+@app.post("/")
+def add_item(request: dict):
+    db = DatabaseMD.DatabaseMD()
+    db.send(request)
+    return {"status": "ok"}
 
-    # Create jsonHandler to load/export
-    jsonhandler = JsonHandler()
-    
-    # Create databaseMD to send to mongo
-    db = DatabaseMD()
-
-    # load json assuming the file is
-    # stored here and is called input.json
-    jsonhandler.load_json("input.json")
-
-    # If you want to send to MongoDB you would run this
-    db.send(jsonhandler.getDictionary())
-
-    # Print what is in the dictionary
-    # print("Loaded Data:")
-    # print(jsonhandler.getDictionary)
-
-    # If you want to export from MongoDB you would run this
-    posts = db.get()
-    # Posts contains an array of all the cards (stored as dicts)
-
-    # Save data to a new Json File
-    jsonhandler.export_json("output.json")
+@app.get("/")
+def get_items():
+    db = DatabaseMD.DatabaseMD()
+    return db.get()

b153d4ea88d1e60018fc1a7603e6478bdfe460f8
Rajarshi Tummala <tummalarajarshi1@gmail.com>
2024-09-14 15:28:24 -0400

    updated database class

---
+++ src/DatabaseMD.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+
+class DatabaseMD:
+    uri = "mongodb+srv://trajarshi:vthacks@vthacks.td4ds.mongodb.net/?retryWrites=true&w=majority&appName=VTHacks"
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    db = client.vthacks
+    col = db.vthacks
+
+    def __init__(self):
+        self.uri = "mongodb+srv://trajarshi:vthacks@vthacks.td4ds.mongodb.net/?retryWrites=true&w=majority&appName=VTHacks"
+        # Create a new client and connect to the server
+        self.client = MongoClient(self.uri)
+        self.db = self.client.vthacks
+        self.col = self.db.vthacks
+
+    # Send a dictionary "doc" to mongodb
+    def send(self,doc):
+        self.col.insert_one(doc)
+
+    # Retrieve all cards and send an array of dict
+    def get(self):
+        posts = []
+        for i in self.col.find():
+            posts.append(i)
+        return posts

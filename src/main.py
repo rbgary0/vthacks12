bedac29ca2526175913bada691247d05a3b81695
creeperjbl24 <75397818+creeperjbl24@users.noreply.github.com>
2024-09-14 14:12:55 -0400

    JsonHandler created

--- src/main.py
+++ src/main.py
@@ -1,20 +1,17 @@
-import Card
-#idk you insert data through args
-def main():
-    # Input a list of parameters separated by commas
-    cardInput = input("Enter the card details separated by commas: ").split(",")
+import JsonHandler
 
-    # Check if input is valid
-    for i in range(6):
-        if cardInput[i] == "":
-            print("Invalid input, one of the fields is empty!")
-            return
+class main():
+
+    # Create jsonHandler to load/export
+    jsonhandler = JsonHandler()
+
+    # load json assuming the file is
+    # stored here and is called input.json
+    jsonhandler.load_json("input.json")
+
+    # Print what is in the dictionary
+    # print("Loaded Data:")
+    # print(jsonhandler.getDictionary)
     
-    # Create Card object
-    # 1: String location
-    # 2: String date
-    # 3: String (int/float?) time
-    # 4: float gasFee
-    # 5: String (int/float?) estTime
-    # 6: int capacity
-    newEntry = Card(cardInput[0], cardInput[1], cardInput[2], float(cardInput[3]), cardInput[4], int(cardInput[5]))
+    # Save data to a new Json File
+    jsonhandler.export_json("output.json")

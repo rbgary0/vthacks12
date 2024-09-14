6e0116c814926599d059ccd65a968d5f168f768a
Birch <ethantwombly3@gmail.com>
2024-09-14 18:56:20 -0400

    Now Listing takes parameters

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,22 +1,30 @@
 import './Listing.css'
 
 
-function Listing() {
+function Listing({ start, end, date, time }) {
+    // let start = infoList.start;
+    // let end = infoList.end;
+    // let date = infoList.date;
+    // let time = infoList.time;
 
 
     return (
         <button className="Listing">
             <div className="Info">
                 <div className="InfoHeader">From</div>
-                <div className="InfoContents">start</div>
+                <div className="InfoContents">{start}</div>
             </div>
             <div className="Info">
                 <div className="InfoHeader">To</div>
-                <div className="InfoContents">end</div>
+                <div className="InfoContents">{end}</div>
+            </div>
+            <div className="Info">
+                <div className="InfoHeader">On</div>
+                <div className="InfoContents">{date}</div>
             </div>
             <div className="Info">
                 <div className="InfoHeader">At</div>
-                <div className="InfoContents">time</div>
+                <div className="InfoContents">{time}</div>
             </div>
         </button>
     )
--- src/components/ListingsGrid.jsx
+++ src/components/ListingsGrid.jsx
@@ -1,44 +1,21 @@
 import './ListingsGrid.css'
 import Listing from './Listing.jsx'
+// import Button from './Button.jsx'
+import React, { useState } from "react";
 
 
 function ListingsGrid() {
 
+    // const [listings, setListings] = useState([]);
+
+    // const addListing = () => {
+    //     setListings([...listings, <Listing />]);
+    // }
 
     return (
         <div>
             <div className="ListingsGrid">
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
-                <Listing />
+                <Listing start="start" end="end" date="date" time="time" />
                 <Listing />
                 <Listing />
                 <Listing />

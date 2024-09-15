cc48e8283b486eca01333aa442a300fb0015797c
creeperjbl24 <75397818+creeperjbl24@users.noreply.github.com>
2024-09-15 00:24:15 -0400

    Added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import './Navbar.css'
 import Form from './Form.jsx'
+import placeholder from './placeholder.png'; // Import your logo image
 
 function Navbar() {
-
-
     return (
         <nav className="topnav">
-            <button className="buttonOption">Going To</button>
-            <button className="buttonOption">Looking For</button>
-            <Form/>
+            <div className="align-left">
+                <img src={placeholder} alt="Logo" className="logo" />
+            </div>
+            <div id="wrapper">
+                <button className="buttonOption">Going To</button>
+                <button className="buttonOption">Looking For</button>
+                <Form/>
+            </div>
         </nav>
     )
 }
